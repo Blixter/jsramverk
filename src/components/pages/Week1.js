9660a3b94d299632d78714420cfabca14434fe28
Robin Blixter <r.blixter89@gmail.com>
2019-09-03 14:11:57 +0200

    Installed react-markdown and copied the text from README to the Week1-component.

--- src/components/pages/Week1.js
+++ src/components/pages/Week1.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ReadmeMarkdown from '../../README.md';
+import ReactMarkdown from 'react-markdown';
 
-export default function Week1() {
-  return (
-    <div>
-      <h1>Vecka 1</h1>
-      <p>Text här</p>
-    </div>
-  )
+class Week1 extends Component {
+
+  constructor() {
+    super();
+    this.state = { markdown: '' };
+  }
+
+  componentWillMount() {
+    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
+    fetch(ReadmeMarkdown).then(res => res.text()).then(text => this.setState({ markdown: text }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return <ReactMarkdown source={markdown} />;
+  }
 }
+
+export default Week1;
