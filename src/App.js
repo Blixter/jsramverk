c12d2963be874b871fc790a4d77d670c124ad62e
Robin Blixter <r.blixter89@gmail.com>
2019-09-11 14:34:38 +0200

    Added styling to the form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import Me from './components/pages/Me';
 import Report from './components/pages/Report';
+import ValidationForm from './components/pages/ValidationForm';
 
 import './App.css';
 
@@ -15,10 +16,12 @@ class App extends Component {
             <ul>
               <li><Link to="/">Me</Link></li>
               <li><Link to="/reports/week/1">Week 1</Link></li>
+              <li><Link to="/register">Register</Link></li>
             </ul>
           </nav>
           <Route exact path="/" component={Me} />
           <Route path="/reports/week/:week" component={Report} />
+          <Route path="/register" component={ValidationForm} />
         </div>
       </Router>
     );
