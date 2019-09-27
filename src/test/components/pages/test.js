e6552cc8d982deb4c4a30c9c42b7974a7b0999e6
Robin Blixter <r.blixter89@gmail.com>
2019-09-27 12:38:20 +0200

    Added testing

---
+++ src/test/components/pages/test.js
@@ -0,0 +1,94 @@
+/**
+ * Dependency Modules
+ */
+const assert = require('assert');
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+const firefox = require('selenium-webdriver/firefox');
+
+// Application Server
+// local
+// const serverUri = "http://localhost:3000";
+// travis
+const serverUri = "http://localhost:8082"
+const appTitle = "Robin Blixter";
+const By = webdriver.By;
+
+let browser;
+
+function goToNavLink(target) {
+    browser.findElement(By.linkText(target)).then(function (element) {
+        element.click();
+    })
+        .catch(error => console.log(error.message));
+}
+
+function matchUrl(target) {
+    browser.getCurrentUrl().then(function (url) {
+        assert.ok(url.endsWith("" + target));
+    })
+        .catch(error => console.log(error.message));
+}
+
+function assertH1(target) {
+    browser.findElement(By.css("h1")).then(function (element) {
+        element.getText().then(function (text) {
+            assert.equal(text, target);
+        })
+            .catch(error => console.log(error.message));
+    });
+}
+
+
+test.describe('Me-page', function () {
+
+    this.timeout(0);
+
+    beforeEach(function(done) {
+        browser = new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.firefox())
+            .setFirefoxOptions(new firefox.Options().headless())
+            .forBrowser('firefox')
+            .build();
+        browser.get(serverUri);
+        done();
+
+    });
+
+    afterEach(function(done) {
+        browser.quit();
+        done();
+    });
+
+    test.it('Test index', function (done) {
+        browser.getTitle()
+            .then(function (title) {
+                assert.equal(title, appTitle);
+                assert.notEqual(title, "Bananer");
+            })
+            .then(function () {
+                assertH1("Me");
+                matchUrl("");
+            })
+            .then(() => done())
+    })
+
+
+    test.it('Test go to Login', function (done) {
+        goToNavLink("Login");
+
+        assertH1("Log in");
+        matchUrl("/login");
+
+        done();
+    });
+
+    test.it('Test go to Register', function (done) {
+        goToNavLink("Register");
+
+        assertH1("Create Account");
+        matchUrl("/register");
+
+        done();
+    });
+})
